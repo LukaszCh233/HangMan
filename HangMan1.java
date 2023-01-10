3ab496e1e7c408634c091c1fad809f77cd7f7927
LukaszCh233 <103947294+LukaszCh233@users.noreply.github.com>
2023-01-10 17:44:34 +0100

    Add files via upload

---
+++ HangMan1.java
@@ -0,0 +1,142 @@
+import javax.security.sasl.SaslClient;
+import javax.swing.plaf.PanelUI;
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+public class HangMan1 {
+    public static Random random = new Random();
+    public static Scanner scanner = new Scanner(System.in);
+    public static String[] words = {"KOPYTKO", "ELEWACJA", "ESTETYCZNY", "EKSTREMALNY"};
+    public static String word = words[random.nextInt(words.length)];
+    public static char[] wword = word.toCharArray();
+    public static int lifes = 0;
+    public static char[] hw;
+
+
+    public static void main(String[] args) {
+
+        String hidenWord = "";
+        for (int i = 0; i < word.length(); i++) {
+            hidenWord = hidenWord + "*";
+        }
+        hw = hidenWord.toCharArray();
+        System.out.println(hw);
+
+        while (lifes < 7 && hidenWord.contains("*")) {
+
+            System.out.println("Podaj litere: ");
+            char letter = scanner.next().charAt(0);
+
+            game(letter);
+            System.out.println(hw);
+        }
+
+    }
+
+
+    public static void game(char letter) {
+
+
+        for (int i = 0; i < word.length(); i++) {
+            if (wword[i] == letter) {
+                hw[i] = letter;
+            }
+
+        }
+        
+        if (Arrays.equals(hw, wword)) {
+            System.out.println("You win");
+
+
+
+        }
+    }
+    public static void hangmanImage() {
+        if (lifes == 1) {
+            System.out.println("Wrong guess, try again");
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println("___|___");
+            System.out.println();
+        }
+        if (lifes == 2) {
+            System.out.println("Wrong guess, try again");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("___|___");
+        }
+        if (lifes == 3) {
+            System.out.println("Wrong guess, try again");
+            System.out.println("   ____________");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("   | ");
+            System.out.println("___|___");
+        }
+        if (lifes == 4) {
+            System.out.println("Wrong guess, try again");
+            System.out.println("   ____________");
+            System.out.println("   |          _|_");
+            System.out.println("   |         /   \\");
+            System.out.println("   |        |     |");
+            System.out.println("   |         \\_ _/");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("___|___");
+        }
+        if (lifes == 5) {
+            System.out.println("Wrong guess, try again");
+            System.out.println("   ____________");
+            System.out.println("   |          _|_");
+            System.out.println("   |         /   \\");
+            System.out.println("   |        |     |");
+            System.out.println("   |         \\_ _/");
+            System.out.println("   |           |");
+            System.out.println("   |           |");
+            System.out.println("   |");
+            System.out.println("___|___");
+        }
+        if (lifes == 6) {
+            System.out.println("Wrong guess, try again");
+            System.out.println("   ____________");
+            System.out.println("   |          _|_");
+            System.out.println("   |         /   \\");
+            System.out.println("   |        |     |");
+            System.out.println("   |         \\_ _/");
+            System.out.println("   |           |");
+            System.out.println("   |           |");
+            System.out.println("   |          / \\ ");
+            System.out.println("___|___      /   \\");
+        }
+        if (lifes == 7) {
+            System.out.println("GAME OVER!");
+            System.out.println("   ____________");
+            System.out.println("   |          _|_");
+            System.out.println("   |         /   \\");
+            System.out.println("   |        |     |");
+            System.out.println("   |         \\_ _/");
+            System.out.println("   |          _|_");
+            System.out.println("   |         / | \\");
+            System.out.println("   |          / \\ ");
+            System.out.println("___|___      /   \\");
+            System.out.println("GAME OVER! The word was " + word);
+        }
+    }
+}
+
+
+
+
